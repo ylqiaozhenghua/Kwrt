3234a2e5ff4aade2d20f58121c15ad29c7452d86
ylqiaozhenghua <102577315+ylqiaozhenghua@users.noreply.github.com>
2025-06-15 11:08:55 +0800

    Update diy.sh

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -15,3 +15,5 @@ sed -i "s/-stock//g" package/boot/uboot-envtools/files/mediatek_filogic
 sed -i "s/openwrt-mediatek-filogic/kwrt-mediatek-filogic/g" target/linux/mediatek/image/filogic.mk
 sed -i "s/ fitblk / /g" target/linux/mediatek/image/filogic.mk
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
