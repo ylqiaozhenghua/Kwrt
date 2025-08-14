9ae8fbcfd3a9b12b1277cfe42bcd7ae91dd9a8ba
ylqiaozhenghua <102577315+ylqiaozhenghua@users.noreply.github.com>
2025-08-14 17:19:45 +0800

    Add files via upload

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -11,3 +11,7 @@ sed -i "s/DEVICE_MODEL := HC5962$/DEVICE_MODEL := HC5962 \/ B70/" target/linux/r
 sed -i '/# start dockerd/,/# end dockerd/d' .config
 
 sed -i "s/--max-leb-cnt=96/--max-leb-cnt=128/g" target/linux/ramips/image/mt7621.mk
+
+sed -i "s/0xfb0000/0x1fb0000/g" target/linux/ramips/dst/mt7621_thunder_timecloud.mk
+
+sed -i "s/16064k/32448k/g" target/linux/ramips/image/mt7621.mk
